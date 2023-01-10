189340b14fcaf63fb5b7ffffb6e628aed4c0a645
ChuSeongYeub <chseye7@gmail.com>
2023-01-10 14:03:56 +0900

    main

--- job_map/main_new.py
+++ job_map/main_new.py
@@ -1,5 +1,5 @@
-from draw_graph_new  import *
-import pandas as pd
+from draw_graph_new import *
+#import pandas as pd
 
 job = input('직업을 입력해주세요: ')
 
@@ -9,11 +9,12 @@ df = pd.read_csv('data_analysis/job_major_subject.csv')
 '''
 try:
     self.job not in df['job']
-except:
-    print('해당 직업이 데이터베이스에 없습니다.')
+except:통
+    print('해당 직업이 데이터베이스에 없습니다.
+    )
     raise NotImplementedError
 '''
 file_name = input('이미지를 저장할 파일명을 입력해주세요: ')
 g = Graph_Viz(job, df)
 a, b, c, d, e, f = g.set_edge_list(df)
-g.draw_graph(a, b, c, d, e, f, graph_size=10, file_name=file_name)
+g.draw_graph(a, b, c, d, e, f, file_name=file_name)
